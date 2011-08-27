745aac63b16105743bb91b63b3767226221c1868
Ethan Hall <ethankhall@gmail.com>
2011-08-27 08:29:13 -0500

    Updated Version number to 12 Altered the DB to use another table, created another table

--- src/com/kopysoft/chronos/content/Chronos.java
+++ src/com/kopysoft/chronos/content/Chronos.java
@@ -23,15 +23,6 @@ package com.kopysoft.chronos.content;
  *
  */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -40,12 +31,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
 import android.os.Environment;
 import android.util.Log;
-
 import com.kopysoft.chronos.enums.Defines;
-import com.kopysoft.chronos.types.Day;
 import com.kopysoft.chronos.types.HoldNote;
-import com.kopysoft.chronos.types.Note;
 import com.kopysoft.chronos.types.Punch;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Chronos extends SQLiteOpenHelper {
 
@@ -54,14 +50,16 @@ public class Chronos extends SQLiteOpenHelper {
     //0.9 = 7
     //1.0.1 - 1.1.0 = 10
     //1.2.0	= 11
+    //2.0.0 = 12
 
-    private static final int DATABASE_VERSION = 11;
+    private static final int DATABASE_VERSION = 12;
     public static final String TABLE_NAME_CLOCK = "clockactions";
+    public static final String TABLE_NAME_JOBS = "jobs";
     public static final String TABLE_NAME_NOTE = "notes";
     public static final String TABLE_NAME_OTHER = "misc";
     public static final String DATABASE_NAME = "Chronos";
 
-    String insertString = "INSERT INTO " + TABLE_NAME_CLOCK + "(time, actionReason) VALUES (?, ?, ?)";
+    //String insertString = "INSERT INTO " + TABLE_NAME_CLOCK + "(time, actionReason) VALUES (?, ?, ?)";
     String insertNote = "INSERT INTO " + TABLE_NAME_NOTE + "(note_string, time) VALUES (?, ?)";
     public static final String insertLunch = "INSERT INTO " +
             TABLE_NAME_OTHER + "(day, lunchTaken) VALUES (?, ?)";
@@ -73,11 +71,24 @@ public class Chronos extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME_CLOCK +
-                " ( _id INTEGER PRIMARY KEY NOT NULL, time LONG NOT NULL, actionReason INTEGER NOT NULL )");
+                " ( _id INTEGER PRIMARY KEY NOT NULL, " +
+                " time LONG NOT NULL, " +
+                " actionReason INTEGER NOT NULL, " +
+                " jobNumber INTEGER DEFAULT 0 )");
         db.execSQL("CREATE TABLE " + TABLE_NAME_NOTE +
-                " ( _id LONG PRIMARY KEY, note_string TEXT NOT NULL, time LONG NOT NULL )");
+                " ( _id LONG PRIMARY KEY, " +
+                " note_string TEXT NOT NULL, " +
+                " time LONG NOT NULL, " +
+                " jobNumber INTEGER DEFAULT 0 )");
         db.execSQL("CREATE TABLE " + TABLE_NAME_OTHER +
-                " ( _id INTEGER PRIMARY KEY NOT NULL, day LONG NOT NULL, lunchTaken INTEGER NOT NULL ) ");
+                " ( _id INTEGER PRIMARY KEY NOT NULL, " +
+                " day LONG NOT NULL, " +
+                " lunchTaken INTEGER NOT NULL, " +
+                " jobNumber INTEGER DEFAULT 0 ) ");
+        db.execSQL("CREATE TABLE " + TABLE_NAME_JOBS +
+                " ( _id INTEGER PRIMARY KEY NOT NULL, " +
+                " name String NOT NULL, " +
+                " default INTEGER NOT NULL )");
     }
 
     @Override
@@ -105,110 +116,71 @@ public class Chronos extends SQLiteOpenHelper {
             Log.e(TAG, "ERROR: Can not move file");
         }
 
-        Log.d(TAG, "Update");
-        ArrayList<HoldNote> Notes = null;
-        try{
-            Notes = getNotes(db);
-        } catch (SQLiteException e){
+        if(oldVersion < 11) {
+            Log.d(TAG, "Update");
+            ArrayList<HoldNote> Notes;
             try{
                 Notes = getNotes(db);
-            } catch( SQLiteException e2){
-                //throw(e2);
-                Notes = null;
-            }
-        }
-
-        ArrayList<Punch> punches = new ArrayList<Punch>();
-        Cursor cursor = db.query(TABLE_NAME_CLOCK, null,
-                null, null, null, null, "_id desc");
-
-        final int colId = cursor.getColumnIndex("_id");
-        final int colTime = cursor.getColumnIndex("time");
-        final int colAR = cursor.getColumnIndex("actionReason");
-        if (cursor.moveToFirst()) {
-            do {
-
-                long id = cursor.getLong(colId);
-                long time = cursor.getLong(colTime);
-                int type = Defines.REGULAR_TIME;
-                if(colAR != -1){
-                    type = cursor.getInt(colAR);
+            } catch (SQLiteException e){
+                try{
+                    Notes = getNotes(db);
+                } catch( SQLiteException e2){
+                    //throw(e2);
+                    Notes = null;
                 }
-                Punch temp = new Punch(time, Defines.IN, id, type);
-                punches.add(temp);
+            }
+
+            ArrayList<Punch> punches = new ArrayList<Punch>();
+            Cursor cursor = db.query(TABLE_NAME_CLOCK, null,
+                    null, null, null, null, "_id desc");
+
+            final int colId = cursor.getColumnIndex("_id");
+            final int colTime = cursor.getColumnIndex("time");
+            final int colAR = cursor.getColumnIndex("actionReason");
+            if (cursor.moveToFirst()) {
+                do {
+
+                    long id = cursor.getLong(colId);
+                    long time = cursor.getLong(colTime);
+                    int type = Defines.REGULAR_TIME;
+                    if(colAR != -1){
+                        type = cursor.getInt(colAR);
+                    }
+                    Punch temp = new Punch(time, Defines.IN, id, type);
+                    punches.add(temp);
 
 
-            } while (cursor.moveToNext());
+                } while (cursor.moveToNext());
+            }
+
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+
+            dropAll(db);
+
+            for (Punch temp : punches) {
+                temp.setNeedToUpdate(true);
+                temp.removeId();
+                temp.commitToDb(db);
+            }
+
+            if(Notes != null){
+                reloadNotes(db, Notes);
+            }
+        } if (oldVersion == 11){
+            String dbCal1 = "ALTER TABLE " + TABLE_NAME_CLOCK + "ADD COLUMN jobNumber INTEGER DEFAULT 0";
+            String dbCal2 = "ALTER TABLE " + TABLE_NAME_NOTE  + "ADD COLUMN jobNumber INTEGER DEFAULT 0";
+            String dbCal3 = "ALTER TABLE " + TABLE_NAME_OTHER + "ADD COLUMN jobNumber INTEGER DEFAULT 0";
+            String dbCal4 ="CREATE TABLE " + TABLE_NAME_JOBS +
+                    " ( _id INTEGER PRIMARY KEY NOT NULL, " +
+                    " name String NOT NULL, " +
+                    " default INTEGER NOT NULL )";
+            db.execSQL(dbCal1);
+            db.execSQL(dbCal2);
+            db.execSQL(dbCal3);
+            db.execSQL(dbCal4);
         }
-
-        if (cursor != null && !cursor.isClosed()) {
-            cursor.close();
-        }
-
-        dropAll(db);
-
-        for(int i = 0; i < punches.size(); i++){
-            Punch temp = punches.get(i);
-            temp.setNeedToUpdate(true);
-            temp.removeId();
-            temp.commitToDb(db);
-        }
-
-        if(Notes != null){
-            reloadNotes(db, Notes);
-        }
-    }
-
-    /**
-     * This method is intended to be used ONLY for testing purposes!!
-     *
-     */
-    public void replacePunches(ArrayList<Punch> punches){
-
-        SQLiteDatabase db = getWritableDatabase();
-        for(int i = 0; i < punches.size(); i++){
-            Punch temp = punches.get(i);
-            temp.setNeedToUpdate(true);
-            temp.removeId();
-            temp.commitToDb(db);
-        }
-        db.close();
-    }
-
-    /**
-     * This method is intended to be used ONLY for testing purposes!!
-     *
-     * @return a list of the punches, so it is able to be restored
-     */
-    public ArrayList<Punch> getPunces(){
-
-        SQLiteDatabase db = getReadableDatabase();
-        ArrayList<Punch> punches = new ArrayList<Punch>();
-        Cursor cursor = db.query(TABLE_NAME_CLOCK, new String[] { "_id","punch_type", "time" },
-                null, null, null, null, "_id desc");
-
-        final int colId = cursor.getColumnIndex("_id");
-        final int colTime = cursor.getColumnIndex("time");
-        final int colType = cursor.getColumnIndex("punch_type");
-        if (cursor.moveToFirst()) {
-            do {
-
-                long id = cursor.getLong(colId);
-                long time = cursor.getLong(colTime);
-                int type = cursor.getInt(colType);
-                Punch temp = new Punch(time, type, id, Defines.REGULAR_TIME);
-                punches.add(temp);
-
-
-            } while (cursor.moveToNext());
-        }
-
-        if (cursor != null && !cursor.isClosed()) {
-            cursor.close();
-        }
-        db.close();
-        return punches;
-
     }
 
     private void reloadNotes(SQLiteDatabase db, ArrayList<HoldNote> Notes){
@@ -229,45 +201,7 @@ public class Chronos extends SQLiteOpenHelper {
     //---------------------------------------------------------------
 
     /**
-     *
-     * @param xml_info List of XMLDay's from XML file
-     *
-     * @note This will destroy the DB. Use carefully!
-     */
-    public void replaceFromXML(List<Day> xml_info, Object context){
-        SQLiteDatabase db = getWritableDatabase();
-        //delete DBgetApplicationContext
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_CLOCK);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_NOTE);
-
-        //Create DB
-        db.execSQL("CREATE TABLE " + TABLE_NAME_CLOCK +
-                " ( _id INTEGER PRIMARY KEY, punch_type INTEGER NOT NULL, time LONG NOT NULL )");
-        db.execSQL("CREATE TABLE " + TABLE_NAME_NOTE +
-                " ( _id INTEGER PRIMARY KEY, note_string TEXT NOT NULL, time LONG NOT NULL )");
-
-        for(int i = 0; i < xml_info.size(); i++){
-            Day workingDay = xml_info.get(i);
-            if(workingDay.getNote(false).trim() != ""){
-                int[] dayTime = { workingDay.getDay()[0],
-                        workingDay.getDay()[1], workingDay.getDay()[2]};
-
-                Note newNote = null;
-                if(context.getClass().equals(Chronos.class) == true){
-                    newNote = new Note(dayTime,(Chronos)context);
-                } else if(context.getClass().equals(Context.class) == true) {
-                    newNote = new Note(dayTime,(Context)context);
-                } else {
-                    return;
-                }
-                newNote.setNote(workingDay.getNote(true));
-                newNote.update();
-            }
-            workingDay.forceWrite();
-        }
-    }
-
-    /**
+     * @param db database
      *
      * @return Returns list of HoldNote's
      */
@@ -279,7 +213,7 @@ public class Chronos extends SQLiteOpenHelper {
 
         long time_temp;
         String text_temp;
-        HoldNote tempNote = null;
+        HoldNote tempNote;
 
         if (cursor.moveToFirst()) {
             do {
@@ -294,74 +228,12 @@ public class Chronos extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
 
-        if (cursor != null && !cursor.isClosed()) {
+        if (!cursor.isClosed()) {
             cursor.close();
         }
         return returnValue;
     }
 
-    /**
-     * Checks to see if the user has stayed clocked in over midnight and will clock them out
-     */
-    /*
-     public boolean checkForMidnight(){
-         GregorianCalendar cal = new GregorianCalendar();
-         SQLiteDatabase db = getWritableDatabase();
-
-         boolean returnValue = false;
-
-         int[] date1 = new int[2];
-         int[] date2 = new int[2];
-
-         date1[0] = cal.get(Calendar.DAY_OF_YEAR);
-         date1[1] = cal.get(Calendar.YEAR);
-
-         Cursor cursor = db.query(TABLE_NAME_CLOCK, new String[] { "punch_type", "time" },
-                 null, null, null,
-                 null, "time ASC ");	//Get all time punches between today at midnight and midnight
-         if(cursor.moveToLast()){
-             int punch = (int)cursor.getLong(0);
-             long time = cursor.getLong(1);
-             if ( punch == Defines.IN ){
-                 cal.setTimeInMillis(time);
-                 date2[0] = cal.get(Calendar.DAY_OF_YEAR);
-                 date2[1] = cal.get(Calendar.YEAR);
-                 Punch newPunch = null;
-
-                 //Check if they are the same
-                 //if the day of year are not the same or years are not the same
-                 if( ( date2[0] != date1[0]) || (date2[1] != date1[1]) ){
-                     cal.set(Calendar.HOUR_OF_DAY, 23);
-                     cal.set(Calendar.MINUTE, 59);
-                     cal.set(Calendar.SECOND, 59);
-                     cal.set(Calendar.MILLISECOND, 0);
-                     long temp = cal.getTimeInMillis();
-                     newPunch = new Punch()
-
-                     //insertTime(db, Defines.OUT, temp);
-
-                     cal.add(Calendar.DAY_OF_YEAR, 1);
-                     cal.set(Calendar.HOUR_OF_DAY, 0);
-                     cal.set(Calendar.MINUTE, 0);
-                     cal.set(Calendar.SECOND, 1);
-                     cal.set(Calendar.MILLISECOND, 0);
-                     date2[0] = cal.get(Calendar.DAY_OF_YEAR);
-                     date2[1] = cal.get(Calendar.YEAR);
-
-                     //check to see that the new day is todays
-                     if( ( date2[0] == date1[0]) || (date2[1] == date1[1]) ){
-                         temp = cal.getTimeInMillis();
-                         //insertTime(db, Defines.IN, temp);
-                     }
-                     returnValue = true;
-                 }
-             }
-         }
-         cursor.close();
-         db.close();
-         return returnValue;
-     }
-      */
 
     /**
      * Drops TABLE_NAME and then recreates the database
@@ -372,6 +244,7 @@ public class Chronos extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_CLOCK);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_NOTE);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_OTHER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_JOBS);
         onCreate(db);
         db.close();
     }
@@ -381,53 +254,8 @@ public class Chronos extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_CLOCK);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_NOTE);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_OTHER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_JOBS);
         onCreate(db);
-    }
-
-
-    /**
-     * Prints all entries in the database
-     */
-    public void printAll(){
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME_CLOCK, new String[] { "_id","punch_type", "time" },
-                null, null, null, null, "_id desc");
-        long arg1, arg2, arg3;
-        int hour, min, sec, month, day, year;
-        GregorianCalendar cal = new GregorianCalendar();
-
-        int colType = cursor.getColumnIndex("punch_type");
-        int colTime = cursor.getColumnIndex("time");
-        int colId = cursor.getColumnIndex("_id");
-
-        if ( Defines.DEBUG_PRINT )Log.d(TAG, "Print All: ");
-
-        if (cursor.moveToFirst()) {
-            do {
-                arg1 = cursor.getLong(colType);
-                arg2 = cursor.getLong(colTime);
-                arg3 = cursor.getLong(colId);
-
-                cal.setTimeInMillis(arg2);
-                hour = cal.get(Calendar.HOUR_OF_DAY);
-                min = cal.get(Calendar.MINUTE);
-                sec = cal.get(Calendar.SECOND);
-
-                day = cal.get(Calendar.DAY_OF_MONTH);
-                month = cal.get(Calendar.MONTH);
-                year = cal.get(Calendar.YEAR);
-
-                if ( Defines.DEBUG_PRINT )Log.d(TAG, "ID: " + arg3 + "\tTime:" + arg2 + "\tPunch Type: " +
-                        arg1 + "\t" + month + "/" + day +
-                        "/" + year + " - " + hour + ":" + min + ":" + sec);
-            } while (cursor.moveToNext());
-            cursor.close();
-        }
-
-        if (cursor != null && !cursor.isClosed()) {
-            cursor.close();
-        }
-        db.close();
     }
 
     /**
