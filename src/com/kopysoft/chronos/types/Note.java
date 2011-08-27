b2dfdbc93fc0bb2a20d116e139ff4423d9a94336
Ethan Hall <ethankhall@gmail.com>
2011-08-27 08:49:49 -0500

    Updated for new DB

--- src/com/kopysoft/chronos/types/Note.java
+++ src/com/kopysoft/chronos/types/Note.java
@@ -2,17 +2,17 @@ package com.kopysoft.chronos.types;
 
 /**
  * 			Copyright (C) 2011 by Ethan Hall
- * 
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  * 	in the Software without restriction, including without limitation the rights
  * 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * 	copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in
  *  all copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@ package com.kopysoft.chronos.types;
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
- *  
+ *
  */
 
 import java.util.GregorianCalendar;
@@ -36,105 +36,109 @@ import com.kopysoft.chronos.content.StaticFunctions;
 import com.kopysoft.chronos.enums.Defines;
 
 public class Note {
-	
-	String noteString;
-	boolean needToUpdate = false;
-	Chronos i_chrono;
-	int[] i_date;
-	
-	String insertNote = "INSERT INTO " + Chronos.TABLE_NAME_NOTE + "(note_string, time) VALUES (?, ?)";
-	
-	public Note(int[] date, Context context){
-		i_chrono = new Chronos(context);
-		i_date = date;
-		noteString = "";
-		getNote(true);
-	}
-	
-	public Note(int[] date, Chronos chronos){
-		i_chrono = chronos;
-		i_date = date;
-		noteString = "";
-		getNote(true);
-	}
-	
-	public synchronized String getNote(boolean force){
-		if(noteString.equalsIgnoreCase("") == false && force == false){
-			return noteString;
-		}
 
-		SQLiteDatabase db = i_chrono.getReadableDatabase();
-		GregorianCalendar cal = new GregorianCalendar(i_date[0], i_date[1], i_date[2]);
-		Cursor cursor = db.query(Chronos.TABLE_NAME_NOTE, new String[] { "note_string" }, 
-				"time = ?", 
-				new String[] {Long.toString(cal.getTimeInMillis())}, 
-				null, null, "time ASC ");
+    String noteString;
+    boolean needToUpdate = false;
+    Chronos i_chrono;
+    int[] i_date;
+    int i_jobNumber;
 
-		if (cursor.moveToFirst()) {
-			do {
-				final int colNote = cursor.getColumnIndex("note_string");
-				noteString = cursor.getString(colNote);
+    String insertNote = "INSERT INTO " + Chronos.TABLE_NAME_NOTE + "(note_string, time, jobNumber) VALUES (?, ?, ?)";
 
-				StaticFunctions.printLog(Defines.ALL, "Chronos - Note", "Note: " + noteString);
-			} while (cursor.moveToNext());
-		}
+    public Note(int[] date, int jobNumber,  Context context){
+        i_chrono = new Chronos(context);
+        i_date = date;
+        noteString = "";
+        getNote(true);
+        i_jobNumber = jobNumber;
+    }
 
-		if (cursor != null && !cursor.isClosed()) {
-			cursor.close();
-		}
-		db.close();
+    public Note(int[] date, int jobNumber, Chronos chronos){
+        i_chrono = chronos;
+        i_date = date;
+        noteString = "";
+        getNote(true);
+        i_jobNumber = jobNumber;
+    }
 
-		return noteString;
-	}
-	
-	public void setNote(String message){
-		if( message.equalsIgnoreCase(noteString) == false)
-			needToUpdate = true;
-		noteString = message;
-	}
-	
-	public void update(){
-		if(needToUpdate == true){
-			editNoteInDb(noteString);
-		}
-	}
-	
-	private synchronized void editNoteInDb(String textString){
-		SQLiteDatabase db = i_chrono.getWritableDatabase();
-		GregorianCalendar cal = new GregorianCalendar(i_date[0], i_date[1], i_date[2]);
-		long id = 0;
+    public synchronized String getNote(boolean force){
+        if(!noteString.equalsIgnoreCase("") && !force){
+            return noteString;
+        }
 
-		Cursor cursor = db.query(Chronos.TABLE_NAME_NOTE, new String[] { "_id" }, 
-				"time = ?", 
-				new String[] {Long.toString(cal.getTimeInMillis())}, 
-				null, null, "time ASC ");
-		//No entry exists
-		if(cursor.getCount() == 0){
-			SQLiteStatement insertStmt = db.compileStatement(insertNote);
+        SQLiteDatabase db = i_chrono.getReadableDatabase();
+        GregorianCalendar cal = new GregorianCalendar(i_date[0], i_date[1], i_date[2]);
+        Cursor cursor = db.query(Chronos.TABLE_NAME_NOTE, new String[] { "note_string" },
+                "time = ? AND jobNumber = ?",
+                new String[] {Long.toString(cal.getTimeInMillis()), Integer.toString(i_jobNumber)},
+                null, null, "time ASC ");
 
-			insertStmt.bindString(1, textString);
-			insertStmt.bindLong(2, cal.getTimeInMillis());
-			insertStmt.executeInsert();
+        if (cursor.moveToFirst()) {
+            do {
+                final int colNote = cursor.getColumnIndex("note_string");
+                noteString = cursor.getString(colNote);
 
-		} else{
-			final int colId = cursor.getColumnIndex("_id");
-			if (cursor.moveToFirst()) {
+                StaticFunctions.printLog(Defines.ALL, "Chronos - Note", "Note: " + noteString);
+            } while (cursor.moveToNext());
+        }
 
-				StaticFunctions.printLog(Defines.ALL, "Chronos - Note", "Insert Note");
-				id = cursor.getLong(colId);
+        if (!cursor.isClosed()) {
+            cursor.close();
+        }
+        db.close();
 
-				ContentValues newConent = new ContentValues();
-				newConent.put("note_string", textString);
+        return noteString;
+    }
 
-				db.update(Chronos.TABLE_NAME_NOTE, newConent, 
-						" _id = ?", new String[] { Long.toString(id) } );
-			}
-		}
+    public void setNote(String message){
+        if( !message.equalsIgnoreCase(noteString))
+            needToUpdate = true;
+        noteString = message;
+    }
 
-		if (cursor != null && !cursor.isClosed()) {
-			cursor.close();
-		}
-		db.close();
-	}
+    public void update(){
+        if(needToUpdate ){
+            editNoteInDb(noteString);
+        }
+    }
+
+    private synchronized void editNoteInDb(String textString){
+        SQLiteDatabase db = i_chrono.getWritableDatabase();
+        GregorianCalendar cal = new GregorianCalendar(i_date[0], i_date[1], i_date[2]);
+        long id;
+
+        Cursor cursor = db.query(Chronos.TABLE_NAME_NOTE, new String[] { "_id" },
+                "time = ? AND jobNumber = ?",
+                new String[] {Long.toString(cal.getTimeInMillis()), Integer.toString(i_jobNumber)},
+                null, null, "time ASC ");
+        //No entry exists
+        if(cursor.getCount() == 0){
+            SQLiteStatement insertStmt = db.compileStatement(insertNote);
+
+            insertStmt.bindString(1, textString);
+            insertStmt.bindLong(2, cal.getTimeInMillis());
+            insertStmt.bindLong(3, i_jobNumber);
+            insertStmt.executeInsert();
+
+        } else{
+            final int colId = cursor.getColumnIndex("_id");
+            if (cursor.moveToFirst()) {
+
+                StaticFunctions.printLog(Defines.ALL, "Chronos - Note", "Insert Note");
+                id = cursor.getLong(colId);
+
+                ContentValues newConent = new ContentValues();
+                newConent.put("note_string", textString);
+
+                db.update(Chronos.TABLE_NAME_NOTE, newConent,
+                        " _id = ?", new String[] { Long.toString(id) } );
+            }
+        }
+
+        if (!cursor.isClosed()) {
+            cursor.close();
+        }
+        db.close();
+    }
 
 }
