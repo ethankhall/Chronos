ab61422be3acde10eb60b8f35346a22fb8ac8639
Ethan Hall <ethankhall@gmail.com>
2012-01-07 15:29:02 -0600

    Migrated from ClockViewer to FragmentClockViewer. Moved functionality from ClockViewer into TodayDropDownFragment, TodayIndivdualFragment, TodayPairFragment, TodaySummaryFragment

---
+++ ChronosApp/src/com/kopysoft/chronos/fragments/ClockFragments/TodaySummaryFragment.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Ethan Hall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package com.kopysoft.chronos.fragments.ClockFragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import com.kopysoft.chronos.adapter.clock.TodayAdapterSummary;
+import com.kopysoft.chronos.content.Chronos;
+import com.kopysoft.chronos.view.RowElement;
+
+public class TodaySummaryFragment extends Fragment {
+
+    public static TodaySummaryFragment newInstance() {
+        TodaySummaryFragment f = new TodaySummaryFragment();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("position", 0);
+        f.setArguments(args);
+
+        return f;
+    }
+    
+    private int position = 0;
+    private final String argumentString = "position";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        position = getArguments() != null ? getArguments().getInt(argumentString) : 0;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(argumentString, position);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        //Log.d(TAG, "Position: " + position);
+        LinearLayout layout = new LinearLayout( getActivity() );
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        Chronos chrono = new Chronos(getActivity());
+        ListView retView = new ListView( getActivity() );
+        BaseAdapter adapter;
+
+
+        RowElement header = new RowElement( getActivity() );
+        header.left().setText("Time");
+        header.center().setText("");
+        header.right().setText("Type");
+        //retView.addHeaderView(header);
+        layout.addView(header, 0);
+        layout.addView(retView, 1);
+
+        adapter = new TodayAdapterSummary(getActivity(), chrono.getAllPunches());
+        retView.setAdapter( adapter );
+        retView.setSelection( position );
+
+        chrono.close();
+
+        return layout;
+
+    }
+
+
+}
---
+++ ChronosApp/src/com/kopysoft/chronos/fragments/FragmentClockViewer.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Ethan Hall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package com.kopysoft.chronos.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import com.kopysoft.chronos.fragments.ClockFragments.TodayDropDownFragment;
+import com.kopysoft.chronos.fragments.ClockFragments.TodayIndividualFragment;
+import com.kopysoft.chronos.fragments.ClockFragments.TodayPairFragment;
+import com.kopysoft.chronos.fragments.ClockFragments.TodaySummaryFragment;
+import com.viewpagerindicator.TitleProvider;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FragmentClockViewer extends FragmentPagerAdapter implements TitleProvider {
+
+    private List<Fragment> fragments;
+    private static String[] titles = new String[] { "Demo 1", "Demo 2", "Demo 3", "Demo 4" };
+
+    public FragmentClockViewer(FragmentManager fm) {
+        super(fm);
+
+        fragments = new LinkedList<Fragment>();
+        fragments.add(TodayPairFragment.newInstance());
+        fragments.add(TodayIndividualFragment.newInstance());
+        fragments.add(TodaySummaryFragment.newInstance());
+        fragments.add(TodayDropDownFragment.newInstance());
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public String getTitle(int i) {
+        return titles[i];
+    }
+}
